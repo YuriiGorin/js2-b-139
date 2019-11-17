b828be8af053cc647c5a2dac25e4d4473e226f50
CW <uragorin@yandex.ru>
2019-11-17 22:24:38 +0300

    добавлены примеры 6-го занятия

--- lesson-05/closures.js
+++ lesson-05/closures.js
@@ -1,3 +1,4 @@
+"use strict";
 let test = 0;
 
 const makeCounter = function() {
@@ -25,3 +26,28 @@ fn2();
 console.log(fn2());
 
 console.log(test);
+
+const sum = function(n) {
+    let acc = 0;
+    if (typeof n !== "undefined" && !isNaN(n)) {
+        acc = n;
+        const fn = function(n) {
+            if (typeof n !== "undefined" && !isNaN(n)) {
+                acc += n;
+                return fn;
+            }
+
+            return acc;
+        };
+
+        return fn;
+    }
+
+    throw new TypeError("Аргумент должен быть числом");
+};
+
+
+console.log(sum(5)(2)(3)());
+
+const coords = { x: 100, y: 150 };
+delete coords.z;
